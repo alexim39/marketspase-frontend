4baa2fc5b2430a8b40d3888d9766449c2ade2166
Alex Imenwo <aleximenwo@gmail.com>
2025-10-16 21:30:20 +0100

    updated promotion emtpy message

--- projects/platform/src/app/promoter/promoter-landing/components/empty-state/empty-state.component.ts
+++ projects/platform/src/app/promoter/promoter-landing/components/empty-state/empty-state.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'empty-state',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatButtonModule],
   template: `
     <div class="empty-state">
       <i class="material-icons empty-icon">campaign</i>
       <h3>No campaigns available</h3>
-      <p>Check back later for new promotion opportunities</p>
+      <p>Check back later for new promotion opportunities or change your ads preferences settings</p>
+      <a class="setting-button" mat-raised-button routerLink="/dashboard/settings/system">
+        <i class="material-icons">arrow_forward</i> Change Settings
+      </a>
     </div>
   `,
   styleUrls: ['./empty-state.component.scss']
