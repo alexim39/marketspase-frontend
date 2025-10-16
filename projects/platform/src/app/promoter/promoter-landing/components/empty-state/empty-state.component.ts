afc638044241034f6198f1755f6599523d83472b
Alex Imenwo <aleximenwo@gmail.com>
2025-10-16 21:31:09 +0100

    update prmotion empty message

--- projects/platform/src/app/promoter/promoter-landing/components/empty-state/empty-state.component.ts
+++ projects/platform/src/app/promoter/promoter-landing/components/empty-state/empty-state.component.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'empty-state',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule ],
   template: `
     <div class="empty-state">
       <i class="material-icons empty-icon">campaign</i>
       <h3>No campaigns available</h3>
       <p>Check back later for new promotion opportunities or change your ads preferences settings</p>
       <a class="setting-button" mat-raised-button routerLink="/dashboard/settings/system">
-        <i class="material-icons">arrow_forward</i> Change Settings
+        <mat-icon class="material-icons">settings_b_roll</mat-icon> Change Settings
       </a>
     </div>
   `,
