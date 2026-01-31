b49646b2f6c402ebbb3e31d43c4ea59fa5722e12
Alex Imenwo <aleximenwo@gmail.com>
2026-01-31 05:43:46 +0100

    remove animation module on platform

--- projects/platform/src/app/app.config.ts
+++ projects/platform/src/app/app.config.ts
@@ -4,10 +4,9 @@ import {
   provideZonelessChangeDetection 
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+//import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-
 // Import Firebase and AngularFire modules
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
@@ -21,7 +20,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     // 1. Initialize and provide the Firebase App
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAnimationsAsync(),
+    //provideAnimationsAsync(),
 
     // 2. Initialize and provide the Firebase Authentication service
     provideAuth(() => getAuth()),
